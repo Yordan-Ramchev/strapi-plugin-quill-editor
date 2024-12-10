8960cfacdb1a15b338ddb4857b6d12b854da5cff
yordan.ramchev <me@yramchev.com>
2024-12-10 20:38:12 +0200

    feat: register plugin on the server

--- server/src/register.ts
+++ server/src/register.ts
@@ -1,7 +1,16 @@
 import type { Core } from '@strapi/strapi';
+import { PLUGIN_ID } from "../../admin/src/pluginId";
 
 const register = ({ strapi }: { strapi: Core.Strapi }) => {
-  // register phase
+  strapi.customFields.register({
+    name: "QuillEditor",
+    plugin: PLUGIN_ID,
+    type: "richtext",
+    inputSize: {
+      default: 12,
+      isResizable: true,
+    },
+  });
 };
 
 export default register;
